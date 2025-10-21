9524c9547e468499ce80ad1182572b45907aeed0
zhengxh <1103888478@qq.com>
2025-10-21 17:49:19 +0800

    test

--- src/main/java/com/zxh/poddome/controller/TestController.java
+++ src/main/java/com/zxh/poddome/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/v1")
+@RequestMapping("/v3")
 public class TestController {
     @GetMapping("/test")
     public String test() {
